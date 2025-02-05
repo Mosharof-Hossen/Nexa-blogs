33637fa164c43805182c4e3b0e7392ed0e6a900b
Mosharof-Hossen <mosharofhossen1998@gmail.com>
2025-02-05 19:06:57 +0600

    Generating Dynamic Metadata with generateMetadata()

---
+++ src/app/blogs/create/page.tsx
@@ -0,0 +1,12 @@
+import BlogForm from '@/components/ui/BlogForm';
+import React from 'react';
+
+const CreateBlog = () => {
+    return (
+        <div>
+            <BlogForm></BlogForm>
+        </div>
+    );
+};
+
+export default CreateBlog;
